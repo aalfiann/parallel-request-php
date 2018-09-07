05191d7da0e4c6d5351b5381131959cff970a7b8
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-09-07 17:10:48 +0700

    - First commit

---
+++ README.md
@@ -0,0 +1,150 @@
+# ParallelRequest PHP
+
+[![Version](https://img.shields.io/badge/stable-1.0.0-green.svg)](https://github.com/aalfiann/parallel-request-php)
+[![Total Downloads](https://poser.pugx.org/aalfiann/parallel-request-php/downloads)](https://packagist.org/packages/aalfiann/parallel-request-php)
+[![License](https://poser.pugx.org/aalfiann/parallel-request-php/license)](https://github.com/aalfiann/parallel-request-php/blob/HEAD/LICENSE.md)
+
+A PHP class to create multiple request in parallel.
+
+## Installation
+
+Install this package via [Composer](https://getcomposer.org/).
+
+1. For the first time project, you have to create the `composer.json` file, (skip to point 2, if you already have `composer.json`)  
+```
+composer init
+```
+
+2. Install
+```
+composer require "aalfiann/parallel-request-php:^1.0"
+```
+
+3. Done, for update in the future you can just run
+```
+composer update
+```
+
+## Usage send GET request
+
+This will send GET request silently without response output.
+
+```php
+require_once ('vendor/autoload.php');
+use \aalfiann\ParallelRequest;
+
+$req = new ParallelRequest;
+$req->request = ['https://jsonplaceholder.typicode.com/posts/1','https://jsonplaceholder.typicode.com/posts/2'];
+$req->options = [
+    CURLOPT_NOBODY => false,
+    CURLOPT_HEADER => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false
+];
+$req->send();
+```
+
+If you want to see the response
+```php
+echo var_dump($req->send()->getResponse());
+```
+
+## Usage send POST request
+
+```php
+require_once ('vendor/autoload.php');
+use \aalfiann\ParallelRequest;
+
+$req = new ParallelRequest;
+$req->request = [
+        [
+            'url' => 'https://jsonplaceholder.typicode.com/posts',
+            'post' => [
+                'title' => 'foo 1',
+                'body' => 'bar 1',
+                'userId' => 1
+            ]
+        ],
+        [
+            'url' => 'https://jsonplaceholder.typicode.com/posts',
+            'post' => [
+                'title' => 'foo 2',
+                'body' => 'bar 2',
+                'userId' => 2
+            ]
+        ]
+    ];
+$req->options = [
+    CURLOPT_NOBODY => false,
+    CURLOPT_HEADER => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false
+];
+
+echo var_dump($req->send()->getResponse());
+```
+
+## Usage send for custom request
+If you want to send custom request like PUT, PATCH, DELETE, etc.  
+Just add the `CURLOPT_CUSTOMREQUEST => 'PUT'`.  
+```php
+require_once ('vendor/autoload.php');
+use \aalfiann\ParallelRequest;
+$req = new ParallelRequest;
+$req->request = [
+        [
+            'url' => 'https://jsonplaceholder.typicode.com/posts/1',
+            'post' => [
+                'id' => 1,
+                'title' => 'foo 1',
+                'body' => 'bar 1',
+                'userId' => 1
+            ]
+        ],
+        [
+            'url' => 'https://jsonplaceholder.typicode.com/posts/2',
+            'post' => [
+                'id' => 2,
+                'title' => 'foo 2',
+                'body' => 'bar 2',
+                'userId' => 1
+            ]
+        ]
+    ]
+];
+$req->options = [
+    CURLOPT_NOBODY => false,
+    CURLOPT_HEADER => false,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => false,
+    CURLOPT_CUSTOMREQUEST => 'PUT'
+];
+
+echo var_dump($req->send()->getResponse());
+```
+
+## Chain Usage
+You also able to make chain.
+```php
+require_once ('vendor/autoload.php');
+use \aalfiann\ParallelRequest;
+$req = new ParallelRequest;
+echo $req->setRequest('http://jsonplaceholder.typicode.com/posts')->send()->getResponse();
+```
+
+## Function List
+- **setRequest($request)** is to create the request url (string or array with post data).
+- **setOptions($options=array())** is to set the options of CURLOPT.
+- **setHttpStatusOnly($httpStatusOnly=false)** if set to true then output response will converted to http status code.
+- **setDelayTime($time=10000)** is the delay execution time for cpu to take a rest. Default is 10000 (10ms) in microseconds.
+- **send()** is curl are sending the request (silently without any output)
+- **getResponse()** is to get the output response (the return data could be string or array).
+- **getResponseJson()** is to get the output response with json formatted.
+
+**Note:**  
+- If you only create single request, response will return string.
+- This class basically using `curl_multi_exec()` function.
+- If you not specify `$req->options` then it will be use `[CURLOPT_HEADER => false,CURLOPT_RETURNTRANSFER => true]` as default.
---
+++ src/ParallelRequest.php
@@ -0,0 +1,154 @@
+<?php 
+namespace aalfiann;
+    /**
+     * A PHP class to create multiple request in parallel
+     *
+     * @package    ParallelRequest Class
+     * @author     M ABD AZIZ ALFIAN <github.com/aalfiann>
+     * @copyright  Copyright (c) 2018 M ABD AZIZ ALFIAN
+     * @license    https://github.com/aalfiann/parallel-request-php/blob/master/LICENSE.md  MIT License
+     */
+    class ParallelRequest {
+
+        var $request,$delayTime=10000,$httpStatusOnly=false,$options=array(),$response=array();
+        
+        /**
+         * Set request
+         * @param request = input the request url here (string or array)
+         * @return this for chaining purpose
+         */
+        public function setRequest($request){
+            $this->request = $request;
+            return $this;
+        }
+
+        /**
+         * Set cURL options
+         * @param options = create your multiple cURL options into array
+         * @return this for chaining purpose 
+         */
+        public function setOption($options=array()){
+            $this->options = $options;
+            return $this;
+        }
+
+        /**
+         * Set Http Status Only
+         * @param httpStatusOnly = if set to true then the response only display the http status code only
+         * @return this for chaining purpose
+         */
+        public function setHttpStatusOnly($httpStatusOnly=true){
+            $this->httpStatusOnly = $httpStatusOnly;
+            return $this;
+        }
+
+        /**
+         * Set delay time for cpu to take a rest
+         * @param time = input the delay execution time. Default is 10000 (10ms) in microseconds.
+         * @return this for chaining purpose
+         */
+        public function setDelayTime($time=10000){
+            $this->delayTime = $time;
+            return $this;
+        }
+        
+        /**
+         * Send a parallel request with using curl_multi_exec
+         * @return this for chaining purpose
+         */
+        public function send() {
+ 
+            // array of curl handles
+            $curly = array();
+            
+            // data to be returned
+            $result = array();
+            
+            // multi handle
+            $mh = curl_multi_init();
+            
+            // make sure request is array
+            if (is_string($this->request)) $this->request = array($this->request);
+           
+            // loop through data request and create curl handles then add them to the multi-handle
+            foreach ($this->request as $id => $d) {
+                $curly[$id] = curl_init();
+                $url = (is_array($d) && !empty($d['url'])) ? $d['url'] : $d;
+                curl_setopt($curly[$id], CURLOPT_URL, $url);
+                if ($this->httpStatusOnly) curl_setopt($curly[$id], CURLOPT_NOBODY, 1);
+           
+                // contains data to post?
+                if (is_array($d)) {
+                    if (!empty($d['post'])) {
+                        curl_setopt($curly[$id], CURLOPT_POST,       1);
+                        curl_setopt($curly[$id], CURLOPT_POSTFIELDS, $d['post']);
+                    }
+                }
+           
+                // set options?
+                if (!empty($this->options)) {
+                    curl_setopt_array($curly[$id], $this->options);
+                } else {
+                    curl_setopt($curly[$id], CURLOPT_HEADER,         0);
+                    curl_setopt($curly[$id], CURLOPT_RETURNTRANSFER, 1);
+                }
+           
+                curl_multi_add_handle($mh, $curly[$id]);
+            }
+           
+            // execute the handles
+            $running = null;
+            do {
+                $mrc =curl_multi_exec($mh, $running);
+            } while($mrc == CURLM_CALL_MULTI_PERFORM);
+
+            // perform multi select
+            while ($running && $mrc == CURLM_OK) {
+                if (curl_multi_select($mh) == -1) {
+                    // delay time for cpu take a rest in every 10ms
+                    usleep($this->delayTime);
+                }
+            
+                do {
+                    $mrc = curl_multi_exec($mh, $running);
+                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            }
+           
+            // get content and remove handles
+            foreach($curly as $id => $c) {
+                if ($this->httpStatusOnly){
+                    $result[$id] = curl_getinfo($c, CURLINFO_HTTP_CODE);
+                } else {
+                    $result[$id] = curl_multi_getcontent($c);
+                }    
+                curl_multi_remove_handle($mh, $c);
+            }
+           
+            // close when all is done
+            curl_multi_close($mh);
+            if (count($result) <= 1) {
+                $this->response = $result[0];
+            } else {
+                $this->response = $result;
+            }
+            return $this;
+        }
+        
+        /**
+         * Get response from request
+         * @return mixed array or string
+         */
+        public function getResponse(){
+            if(!empty($this->response)) return $this->response;
+            return null;
+        }
+
+        /**
+         * Get response from request with json format
+         * @return json
+         */
+        public function getResponseJson(){
+            if(!empty($this->response)) return json_encode($this->response);
+            return null;
+        }
+    }
